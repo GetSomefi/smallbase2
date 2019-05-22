0c6d097eb128091c6b5a899bba02e27133dc8fa0
GetSomefi <peyte.com@gmail.com>
2019-05-22 23:47:03 +0300

    octicons work now smoothly

--- reactiveSEO_modules/basicLogin.js
+++ reactiveSEO_modules/basicLogin.js
@@ -1,38 +1,99 @@
 console.log('Basic login Init');
 
+//https://medium.com/@Farzad_YZ/3-ways-to-clone-objects-in-javascript-f752d148054d
+function copyTool(src) {
+  return Object.assign({}, src);
+}
+
 function doTask(whattodo,username,password, usernameInput, passwordInput,elMaster) {
   console.log("Wait");
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      console.log("Done");
-      var json = JSON.parse(this.responseText);
-      console.log(json);
+		console.log("Done");
+		var json = JSON.parse(this.responseText);
+		console.log(json);
       
-      let iconOk = document.createElement("div");
-      iconOk.innerHTML = json.userData.iconOk;
-      let iconError = document.createElement("div");
-      iconError.innerHTML = json.userData.iconError;
+      	//reset view
+		/*
+		var elements = document.getElementsByClassName("inputicon");
+		for (var i = 0; i < elements.length; i++) {
+		    elements[i].parentNode.removeChild(elements[i]); 
+		}
+		*/
 
-      console.log('svg', json.userData.iconOk); 
-      usernameInput.classList.remove("username-ok");
-      usernameInput.classList.remove("username-error");
-      passwordInput.classList.remove("password-ok");
-      passwordInput.classList.remove("username-error");
-      if(json.userData.usernameOk){
-      	usernameInput.classList.add("username-ok");
-      	usernameInput.parentNode.insertBefore(iconOk, usernameInput.nextSibling);
-      }else{
-      	usernameInput.classList.add("username-error");
-      	usernameInput.parentNode.insertBefore(iconError, usernameInput.nextSibling);
-      }
-      if(json.userData.passwordOk){
-      	passwordInput.classList.add("password-ok");
-      	passwordInput.parentNode.insertBefore(iconOk, passwordInput.nextSibling);
-      }else{
-		passwordInput.classList.add("username-error");
-		passwordInput.parentNode.insertBefore(iconError, passwordInput.nextSibling);
-      }
+		let iconOk = document.createElement("div");
+		iconOk.classList = "inputicon icon-ok";
+		iconOk.innerHTML = json.userData.iconOk;
+
+		let iconError = document.createElement("div");
+		iconError.classList = "inputicon icon-ok";
+		iconError.innerHTML = json.userData.iconError;
+
+		console.log('svg', json.userData.iconOk); 
+		usernameInput.classList.remove("username-ok");
+		usernameInput.classList.remove("username-error");
+		passwordInput.classList.remove("password-ok");
+		passwordInput.classList.remove("username-error");
+
+		console.log( usernameInput.parentNode.childNodes );
+		let iconOnUsername = usernameInput.parentNode.childNodes[1];
+		if(iconOnUsername){
+			usernameInput.parentNode.removeChild( iconOnUsername );
+		}
+
+		console.log( passwordInput.parentNode.childNodes );
+		let iconOnPassword = passwordInput.parentNode.childNodes[1];
+		if(iconOnPassword){
+			passwordInput.parentNode.removeChild( iconOnPassword );
+		}
+
+		if(json.userData.usernameOk){
+			usernameInput.classList.add("username-ok");
+			//usernameInput.parentNode.insertBefore(iconOk, usernameInput.nextSibling);
+			let ico = iconOk.cloneNode(true); 
+			usernameInput.parentNode.appendChild(ico);
+			
+			setTimeout(function(){
+				ico.classList.add("jumpOutScale");
+			},200);
+		}else{
+			usernameInput.classList.add("username-error");
+			//usernameInput.parentNode.insertBefore(iconError, usernameInput.nextSibling);
+			let ico = iconError.cloneNode(true); 
+			usernameInput.parentNode.appendChild(ico);
+			
+			setTimeout(function(){
+				ico.classList.add("jumpOutScale");
+			},200);
+		}
+		
+		if(json.userData.passwordOk){
+			passwordInput.classList.add("password-ok");
+			//passwordInput.parentNode.insertBefore(iconOk, passwordInput.nextSibling);
+			let ico = iconOk.cloneNode(true); 
+			passwordInput.parentNode.appendChild(ico);
+			
+			setTimeout(function(){
+				ico.classList.add("jumpOutScale");
+			},200);
+		}else{
+			passwordInput.classList.add("password-error");
+			//passwordInput.parentNode.insertBefore(iconError, passwordInput.nextSibling);
+			let ico = iconError.cloneNode(true); 
+			passwordInput.parentNode.appendChild(ico);
+			
+			setTimeout(function(){
+				ico.classList.add("jumpOutScale");
+			},200);
+		}
+		
+		/*
+		setTimeout(function(){
+			iconOk.classList.add("jumpOutScale");
+			iconError.classList.add("jumpOutScale");
+		},200);
+		*/
     }
   };
   var data = {
@@ -69,12 +130,18 @@ class basicLogin extends HTMLElement {
 	el.id="username";
 	el.type = "text";
 
+	let elHolder = document.createElement('div');
+	elHolder.classList = "basic-login-inner-holder login-inner-holder-username";
+
 	let el2 = document.createElement('input');
 	el2.classList = "inputbox input-password";
 	el2.placeholder = "Password";
 	el2.autocomplete = "off";
 	el2.id = "password";
 	el2.type = "password";
+
+	let el2Holder = document.createElement('div');
+	el2Holder.classList = "basic-login-inner-holder login-inner-holder-password";
 
 	let el3 = document.createElement('button');
 	el3.classList = "fancybutton prevent-highlight";
@@ -88,8 +155,11 @@ class basicLogin extends HTMLElement {
 		let loginOk = doTask("checkLogin", username, password, el, el2,elMaster);
 	});
 
-	elMaster.appendChild(el);
-	elMaster.appendChild(el2);
+	elHolder.appendChild(el);
+	el2Holder.appendChild(el2);
+
+	elMaster.appendChild(elHolder);
+	elMaster.appendChild(el2Holder);
 	elMaster.appendChild(el3);
 
 	// Append it to the shadow root
@@ -98,6 +168,8 @@ class basicLogin extends HTMLElement {
 	// Create some CSS to apply to the shadow dom
 	var style = document.createElement('style');
 
+	let okColor = "#8BC34A";
+	let errorColor = "#FF5722";
 	style.textContent =
 	'.inputbox {' +
 		'background-color: #54545400 !important;'+
@@ -122,10 +194,10 @@ class basicLogin extends HTMLElement {
 	'}' +
 
 	'.username-ok, .password-ok {'+
-    	'border-bottom: 1px solid #8BC34A;'+
+    	'border-bottom: 1px solid '+okColor+';'+
 	'}' +
 	'.username-error, .password-error {'+
-    	'border-bottom: 1px solid #FF5722;'+
+    	'border-bottom: 1px solid '+errorColor+';'+
 	'}' +
 
 	'button.fancybutton {' +
@@ -151,7 +223,35 @@ class basicLogin extends HTMLElement {
 	'button.fancybutton:hover {' +
 		'transform: scale(1.05);' +
 		//'' +
-	'}';
+	'}' +
+
+	'.inputicon {' +
+		'float:right;' +
+		'margin-top: -60px;' +
+    	'margin-right: 15px;' +
+    	'background: #ffffff4f;' +
+    	'padding: 8px;' +
+    	'border-radius: 100%;' +
+    	'width: 20px;' +
+    	'height: 20px;' +
+    	'text-align: center;' +
+    	'line-height: 25px;' +
+    	'transition: all .2s linear;' +
+    	'opacity:0;' +
+		//'' +
+	'}' +
+
+	'.jumpOutScale {'+
+		'opacity:1;' +
+		'transform: scale(1.1);' +
+	'}' +
+
+	'svg.octicon.octicon-thumbsup {'+
+		'fill:'+okColor+';' +
+	'}' +
+	'svg.octicon.octicon-thumbsdown {'+
+		'fill:'+errorColor+';' +
+	'}' +
 
 	shadow.appendChild(style);
     
